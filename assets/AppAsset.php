7ece18464f746ff7df8905612ee6d6d4339b4b37
Luisintosh <m@luism.net>
2016-03-15 17:37:01 -0600

    fix assets conflicts

--- assets/AppAsset.php
+++ assets/AppAsset.php
@@ -13,17 +13,17 @@ use yii\web\AssetBundle;
  * @author Qiang Xue <qiang.xue@gmail.com>
  * @since 2.0
  */
-class AppAsset extends AssetBundle
-{
-  public $basePath = '@webroot';
-  public $baseUrl = '@web';
-  public $css = [
-  ];
-  public $js = [
-  ];
-  public $depends = [
-      'yii\web\YiiAsset',
-      'yii\bootstrap\BootstrapAsset',
-      'yii\bootstrap\BootstrapPluginAsset',
-  ];
-}
+ class AppAsset extends AssetBundle
+ {
+   public $basePath = '@webroot';
+   public $baseUrl = '@web';
+   public $css = [
+     'css/style.css',
+   ];
+   public $js = [
+     'js/app.js',
+   ];
+   public $depends = [
+       'yii\web\YiiAsset',
+   ];
+ }
